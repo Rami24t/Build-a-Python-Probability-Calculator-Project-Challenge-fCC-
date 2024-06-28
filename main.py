a8c07c46597fb6d2e17decd764776668513e62a9
Rami <103028944+Rami24t@users.noreply.github.com>
2024-06-28 16:39:55 +0200

    feat: improve __str__ method, and enhance test code formatting

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Hat():
 
     def __str__(self):
         params = ', '.join(f"{color}={self.count[color]}" for color in self.count)
-        return f"{type(self).__name__}({params})\n"
+        return f"{type(self).__name__}({params})"
 
     def draw(self, n):
         removedItems = []
@@ -48,8 +48,9 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 if __name__=='__main__':
     hat1 = Hat(yellow=3, blue=2, green=6)
     hat2 = Hat(red=5, orange=4)
-    hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
-    print(str(hat1), '\n'+str(hat2), '\n'+str(hat3) + '------------------------------------\n')
+    print('hat1:  '+str(hat1)+'\nhat2:  '+str(hat2)+'\n------------------------------------\n')
 
-    print(str(hat3.draw(5)) + '\n')
-    print(hat3)
+    hat3 = Hat(red=5, orange=4, black=1, blue=0, pink=2, striped=9)
+    print('hat3: ', hat3)
+    print('Draw 5 random balls from hat3: ', hat3.draw(5))
+    print('hat3: ', hat3)
