a3a33800655985692cda4ae0c809713460ec9155
Rami <103028944+Rami24t@users.noreply.github.com>
2024-06-28 15:40:34 +0200

    feat: enhance Hat class constructor, add Counter class for contents handling, and improve __str__ method

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 import copy
 import random
 
+
 class Hat():
     def __init__(self, **kwargs):
-        self.kwargs = kwargs
-        self.contents = []
-        for key, value in kwargs.items():
-            for _ in range(value):
-                self.contents.append(key)
+        self.count = kwargs
+        self.contents = Counter.spread(**kwargs)
 
     def __str__(self):
-        params = ', '.join(f"{color}={self.kwargs[color]}" for color in self.kwargs)
+        params = ', '.join(f"{color}={self.count[color]}" for color in self.count)
         return f"{type(self).__name__}({params})\n"
+
+
+class Counter():
+    def spread(**kwargs):
+        spread_contents = []
+        for key, value in kwargs.items():
+            for _ in range(value):
+                spread_contents.append(key)
+        return spread_contents
+    # def count(spread):
+    #     count = {}
+    #    for item in spread:
+    #        if item not in count:
+    #            count[str(item)] = 1
+    #        else:
+    #            count[item] += 1
+    #    return count
+
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     pass
